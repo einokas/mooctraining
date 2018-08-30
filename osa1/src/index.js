bd9a512da188d8b97b081aa8a08d3c241fee185c
Erik <erik.jokela@gmail.com>
2018-08-30 09:53:33 +0300

    new file structure

---
+++ osa1/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Hello = (props) => {
+    return (
+      <div>
+        <p>Hello {props.name}, you are {props.age} years old</p>
+      </div>
+    )
+  }
+  
+  const App = () => {
+      const nimi = 'Pekka'
+      const ika = 10
+    return( 
+        [
+        <h1>Greetings</h1>,
+        <Hello name="Arto" age = {26 + 10} />,
+        <Hello name={nimi} age = {ika} />,
+        <Footer />
+        ]
+    )
+  }
+
+  const Footer = () => {
+    return (
+      <div>greeting app created by <a href="https://github.com/einokas">erik</a></div>
+    )
+  }
+  
+  ReactDOM.render(<App />, document.getElementById('root'))
